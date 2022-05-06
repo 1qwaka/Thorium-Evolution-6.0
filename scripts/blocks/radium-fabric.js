61d1575780d2612580f556fd7fdefbd15993aec2
1qwaka <70287659+1qwaka@users.noreply.github.com>
2022-05-06 13:11:44 +0500

    fixed radium fabric crash game on build

--- scripts/blocks/radium-fabric.js
+++ scripts/blocks/radium-fabric.js
@@ -1,40 +1,46 @@
-const shaders = require ("teshaders");
+const shaders = require("teshaders");
 
-const radiumFabric = extend(GenericSmelter,"radium-fabric",{
+const radiumFabric = extend(GenericSmelter, "radium-fabric", {
   _bottomRegion: null,
   _rotatorRegion: null,
-  load(){
+  load() {
     this.super$load();
     this.topRegion = Core.atlas.find("clear");
-    this._bottomRegion = Core.atlas.find(this.name+"-bottom");
-    this._rotatorRegion = Core.atlas.find(this.name+"-rotator");
+    this._bottomRegion = Core.atlas.find(this.name + "-bottom");
+    this._rotatorRegion = Core.atlas.find(this.name + "-rotator");
   },
-  getR(){
+  getR() {
     return this._rotatorRegion;
   },
-  getB(){
+  getB() {
     return this._bottomRegion;
   },
-  icons(){
-    return [Core.atlas.find(this.name+"-bottom"),
+  icons() {
+    return [Core.atlas.find(this.name + "-bottom"),
     Core.atlas.find(this.name)];
   }
 });
+
 radiumFabric.flameColor = Items.thorium.color;
-radiumFabric.buildType = () => extend(GenericSmelter.SmelterBuild,radiumFabric,{
-    updateTile(){
-        this.super$updateTile();
-        this.totalProgress+=this.warmup;
-    },
-    draw(){
-        Draw.rect(this.block.getB(),this.x,this.y);
-        Draw.alpha(this.warmup);
-        Draw.rect(this.block.getR(),this.x,this.y,this.totalProgress*4);
-        Draw.alpha(1);
-        Draw.sort(false);
-        Draw.shader(shaders.rainbow);
-        this.super$draw();
-        Draw.shader();
-        Draw.sort(true);
-    }
+
+radiumFabric.buildType = () => extend(GenericSmelter.SmelterBuild, radiumFabric, {
+
+  updateTile() {
+    this.super$updateTile();
+    this.totalProgress += this.warmup;
+  },
+
+  draw() {
+    Draw.rect(this.block.getB(), this.x, this.y);
+    Draw.alpha(this.warmup);
+    // Draw.alpha(1);
+    // Draw.sort(false);
+    // Draw.shader(shaders.rainbowGreen);
+    Draw.rect(this.block.getR(), this.x, this.y, this.totalProgress * 4);
+    // Draw.shader();
+    // Draw.sort(true);
+    Draw.alpha(1);
+    this.super$draw();
+  }
+
 });
